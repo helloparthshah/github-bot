c4827f307deaf591350dcf7906df91e7148fe2bf
parthshah01 <helloparthshah@gmail.com>
2020-08-04 17:47:09 +0530

    Test

---
+++ index.js
@@ -0,0 +1,18 @@
+const jsonfile = require('jsonfile');
+const moment = require('moment');
+const simpleGit = require('simple-git')
+
+const FILE_PATH = './data.json';
+
+const DATE = moment().subtract(1, 'y').add(28, 'd').format();
+
+const data = {
+    date: DATE,
+}
+
+jsonfile.writeFile(FILE_PATH, data, () => {
+    //git commit --date=""
+    simpleGit().add([FILE_PATH]).commit(DATE, {
+        '--date': DATE
+    }).push();
+});
